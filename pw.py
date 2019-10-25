0a6a8e84f3feeead52316a014f4a8199589d9d9d
seakun <seakun@users.noreply.github.com>
2019-10-25 16:12:32 -0400

    Python String and Dictionary Manipulation

--- pw.py
+++ pw.py
@@ -4,3 +4,16 @@
 PASSWORDS = { 'email': '6Me1^fLuF!FvBI$C@5!CowhAO!@3N',
               'blog': 'jJCaAo7seIWfpjfWC^35*8%q!A4lO',
               'luggage': '1234567890'}
+
+import sys
+if len(sys.argv) < 2:
+	print('Usage: python pw.py [account] - copy account password')
+	sys.exit()
+
+account = sys.argv[1] # first command line arg is the account name
+
+if account in PASSOWRDS:
+	pyperclip.copy(PASSWORDS[account])
+	print('Password for ' + account + ' copied to clipboard.')
+else:
+	print('There is no account named ' + account)
